5a7447fc712f12ca270cf9b7b3a81ac1deb571cf
Mapelujo <olamideiyanda18@gmail.com>
2023-10-02 16:48:54 +0100

    progress

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ console.log('Hello Kareem');
 const dayInput = document.querySelector('#day');
 const monthInput = document.querySelector('#month');
 const yearInput = document.querySelector('#year');
+const submitBtn = document.querySelector('#submit');
