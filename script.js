699a171236f4e37f863271ff5105ce6c98005a92
Mapelujo <olamideiyanda18@gmail.com>
2023-10-02 16:55:59 +0100

    progress

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const dayInput = document.querySelector('#day');
 const monthInput = document.querySelector('#month');
 const yearInput = document.querySelector('#year');
 const submitBtn = document.querySelector('#submit');
+
+// Implementing the submit functionality when input is empty.
+
+submitBtn.addEventListener('click', function(e){
+    console.log('I was clicked', e.target);
+})
