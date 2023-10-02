8cf073e5271eafbe746e3e1a0450c9f341fc1829
Mapelujo <olamideiyanda18@gmail.com>
2023-10-02 17:43:28 +0100

    progress

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const monthInput = document.querySelector('#month');
 const yearInput = document.querySelector('#year');
 const submitBtn = document.querySelector('#submit');
 const allErrorSpan = document.querySelectorAll('.error-span');
+const allInputs = document.querySelectorAll('.input');
+const allLabels = document.querySelectorAll('label');
 
 // Implementing the submit functionality when input is empty.
 
 submitBtn.addEventListener('click', function(e){
     if (dayInput.value === '' && monthInput.value === '' && yearInput.value === '') {
         allErrorSpan.forEach(span => span.classList.remove('hidden'));
-
+        allInputs.forEach(input => input.style.border = '1px solid hsl(0, 100%, 67%)');
+        allLabels.forEach(label => label.style.color = 'hsl(0, 100%, 67%)');
     }
 })
